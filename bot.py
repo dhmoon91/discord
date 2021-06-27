0e1492a63fa43e970ed7a01e2acc19d41051f57a
jung1411 <jung1411@gmail.com>
2021-06-27 15:53:21 -0400

    removed df_styled.png

--- bot.py
+++ bot.py
@@ -6,80 +6,90 @@ import discord
 from discord.ext import commands
 
 # Riot util func.
-from riot import getSummonerRank,previousMatch
+from riot import getSummonerRank, previousMatch
 
 # saving df to image
 import dataframe_image as dfi
 
 
 intents = discord.Intents.default()
-intents.members = True # Subscribe to the privileged members intent.
+intents.members = True  # Subscribe to the privileged members intent.
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 # TODO: Update prefix to be called with bot name.
-bot = commands.Bot(command_prefix=commands.when_mentioned_or(""), intents=intents)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"), intents=intents)
+
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+    print(f"{bot.user.name} has connected to Discord!")
+
 
 @bot.event
 async def on_member_join(member):
-  # create a direct message channel.
-  await member.create_dm()
-  # Send welcome msg.
-  await member.dm_channel.send(
-      f'Hi {member.name}, welcome to 관전남 월드!'
-  )
-
-@bot.command(name='rank', help='Get rank of summoner')
-async def get_rank(ctx, name:str):
-  summoner_info = getSummonerRank(name)
-  
-  embed = discord.Embed(title = "Solo/Duo Rank", color = discord.Color.dark_gray())
-
-  summoner_name = summoner_info['user_name']
-  
-  # Removing space of the summoner name to access op.gg url of the summoner 
-  summoner_name_opgg = summoner_name.replace(" ", "")
-  # Add author, thumbnail, fields, and footer to the embed
-  embed.set_author(name=summoner_name, url=f"https://na.op.gg/summoner/userName={summoner_name_opgg}", icon_url=summoner_info['summoner_icon_image_url'])
-  
-  # Get image of tier by path
-  tier_image = summoner_info['tier_image']
-  file = discord.File(tier_image)
-  # Need to get the filename in order to attach to the thumbnail
-  tier_image_filename = tier_image.replace('ranked-emblems/', '')
-  # Embed thumbnail image of tier at the side of the embed
-  embed.set_thumbnail(url=f"attachment://{tier_image_filename}")
-
-  # Setting variables for summoner information to display as field
-  summoner_rank = summoner_info['tier']
-  solo_win = summoner_info['solo_win']
-  solo_loss = summoner_info['solo_loss']
-  summoner_total_game = solo_win + solo_loss
-  solo_rank_percentage = int(solo_win / summoner_total_game * 100)
+    # create a direct message channel.
+    await member.create_dm()
+    # Send welcome msg.
+    await member.dm_channel.send(f"Hi {member.name}, welcome to 관전남 월드!")
 
 
-  embed.add_field(name=summoner_rank, 
-  value=f"Total Games Played: {summoner_total_game}\n{solo_win}W {solo_loss}L {solo_rank_percentage}%", 
-  inline=False)
-  await ctx.send(file=file, embed = embed)
+@bot.command(name="rank", help="Get rank of summoner")
+async def get_rank(ctx, name: str):
+    summoner_info = getSummonerRank(name)
 
-@bot.command(name='last_match', help='Get last match history')
-async def get_last_match(ctx, name:str):
-  df = previousMatch(name)
-  dfi.export(df, 'df_styled.png')
-  file = discord.File("df_styled.png")
-  embed = discord.Embed()
-  embed.set_image(url="attachment://df_styled.png")
-  await ctx.send(embed=embed, file=file)
+    embed = discord.Embed(title="Solo/Duo Rank", color=discord.Color.dark_gray())
+
+    summoner_name = summoner_info["user_name"]
+
+    # Removing space of the summoner name to access op.gg url of the summoner
+    summoner_name_opgg = summoner_name.replace(" ", "")
+    # Add author, thumbnail, fields, and footer to the embed
+    embed.set_author(
+        name=summoner_name,
+        url=f"https://na.op.gg/summoner/userName={summoner_name_opgg}",
+        icon_url=summoner_info["summoner_icon_image_url"],
+    )
+
+    # Get image of tier by path
+    tier_image = summoner_info["tier_image"]
+    file = discord.File(tier_image)
+    # Need to get the filename in order to attach to the thumbnail
+    tier_image_filename = tier_image.replace("ranked-emblems/", "")
+    # Embed thumbnail image of tier at the side of the embed
+    embed.set_thumbnail(url=f"attachment://{tier_image_filename}")
+
+    # Setting variables for summoner information to display as field
+    summoner_rank = summoner_info["tier"]
+    solo_win = summoner_info["solo_win"]
+    solo_loss = summoner_info["solo_loss"]
+    summoner_total_game = solo_win + solo_loss
+    solo_rank_percentage = int(solo_win / summoner_total_game * 100)
+
+    embed.add_field(
+        name=summoner_rank,
+        value=f"Total Games Played: {summoner_total_game}\n{solo_win}W {solo_loss}L {solo_rank_percentage}%",
+        inline=False,
+    )
+    await ctx.send(file=file, embed=embed)
+
+
+@bot.command(name="last_match", help="Get last match history")
+async def get_last_match(ctx, name: str):
+    df = previousMatch(name)
+    dfi.export(df, "df_styled.png")
+    file = discord.File("df_styled.png")
+    embed = discord.Embed()
+    embed.set_image(url="attachment://df_styled.png")
+    await ctx.send(embed=embed, file=file)
+    os.remove("df_styled.png")
 
 
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
-        await ctx.send('You do not have the correct role for this command.')
+        await ctx.send("You do not have the correct role for this command.")
+
+
 bot.run(TOKEN)
--- riot.py
+++ riot.py
@@ -4,92 +4,91 @@ import pandas as pd
 import pydash
 
 from dotenv import load_dotenv
+
 load_dotenv()
-RIOTAPIKEY = os.getenv('RIOT_API_KEY')
+RIOTAPIKEY = os.getenv("RIOT_API_KEY")
 
 watcher = LolWatcher(RIOTAPIKEY)
-my_region = 'na1'
+my_region = "na1"
 
 # Get summoner rank.
 def getSummonerRank(name: str):
-  # We need to get id
-  user = watcher.summoner.by_name(my_region, name)
-  ranked_stat= watcher.league.by_summoner(my_region, user['id'])
+    # We need to get id
+    user = watcher.summoner.by_name(my_region, name)
+    ranked_stat = watcher.league.by_summoner(my_region, user["id"])
 
-  # Get summoner Icon Image
-  profileiconid = user['profileIconId']
-  version = watcher.data_dragon.versions_for_region(my_region)['v']
-  summoner_icon_image_url = f"http://ddragon.leagueoflegends.com/cdn/{version}/img/profileicon/{profileiconid}.png"
+    # Get summoner Icon Image
+    profileiconid = user["profileIconId"]
+    version = watcher.data_dragon.versions_for_region(my_region)["v"]
+    summoner_icon_image_url = f"http://ddragon.leagueoflegends.com/cdn/{version}/img/profileicon/{profileiconid}.png"
 
-  user_name = user['name']
-  summoner_level = user['summonerLevel']
+    user_name = user["name"]
+    summoner_level = user["summonerLevel"]
 
+    # Find solo queue data.
+    solo_rank_stat = pydash.find(ranked_stat, {"queueType": "RANKED_SOLO_5x5"})
 
-  # Find solo queue data.
-  solo_rank_stat =  pydash.find(ranked_stat, {"queueType":"RANKED_SOLO_5x5"})
+    tier_division = solo_rank_stat["tier"]
+    tier_rank = solo_rank_stat["rank"]
 
-  tier_division = solo_rank_stat['tier']
-  tier_rank = solo_rank_stat['rank']
+    tier_image = f"ranked-emblems/Emblem_{tier_division}.png"
 
+    tier = " ".join([tier_division, tier_rank])
 
-  tier_image = f"ranked-emblems/Emblem_{tier_division}.png"
+    solo_win = solo_rank_stat["wins"]
+    solo_loss = solo_rank_stat["losses"]
 
-  tier = ' '.join([tier_division, tier_rank])
+    summoner_profile = {
+        "user_name": user_name,
+        "summoner_icon_image_url": summoner_icon_image_url,
+        "summoner_level": summoner_level,
+        "tier_image": tier_image,
+        "tier": tier,
+        "solo_win": solo_win,
+        "solo_loss": solo_loss,
+    }
+    return summoner_profile
 
-  solo_win = solo_rank_stat['wins']
-  solo_loss = solo_rank_stat['losses']
-
-  summoner_profile = {
-    'user_name' : user_name,
-    'summoner_icon_image_url' : summoner_icon_image_url,
-    'summoner_level': summoner_level,
-    'tier_image' : tier_image,
-    'tier': tier,
-    'solo_win': solo_win,
-    'solo_loss': solo_loss,
-  }
-  return summoner_profile
 
 # Get previous match history of summoner.
-def previousMatch(name:str):
-  user = watcher.summoner.by_name(my_region, name)
-  matches = watcher.match.matchlist_by_account(my_region, user['accountId'])
-  last_match = matches['matches'][0]
-  match_detail = watcher.match.by_id(my_region, last_match['gameId'])
+def previousMatch(name: str):
+    user = watcher.summoner.by_name(my_region, name)
+    matches = watcher.match.matchlist_by_account(my_region, user["accountId"])
+    last_match = matches["matches"][0]
+    match_detail = watcher.match.by_id(my_region, last_match["gameId"])
 
-  # check league's latest version
-  latest = watcher.data_dragon.versions_for_region(my_region)['n']['champion']
-  # Get static info.
-  static_champ_list = watcher.data_dragon.champions(latest, False, 'en_US')
-  # champ static data list.
-  champ_dict = {}
-  for key in static_champ_list['data']:
-      row = static_champ_list['data'][key]
-      champ_dict[row['key']] = row['id']
+    # check league's latest version
+    latest = watcher.data_dragon.versions_for_region(my_region)["n"]["champion"]
+    # Get static info.
+    static_champ_list = watcher.data_dragon.champions(latest, False, "en_US")
+    # champ static data list.
+    champ_dict = {}
+    for key in static_champ_list["data"]:
+        row = static_champ_list["data"][key]
+        champ_dict[row["key"]] = row["id"]
 
-  participants = []
-  for row in match_detail['participants']:
-    participants_row = {}
-    # Get summoner name
-    pid = row['participantId']
-    summonerInfo = pydash.find(match_detail['participantIdentities'], {'participantId':  pid})
-    participants_row['Name'] = summonerInfo['player']['summonerName']
-    participants_row['Champion'] = champ_dict[str(row['championId'])]
-    # participants_row['spell1'] = row['spell1Id']
-    # participants_row['spell2'] = row['spell2Id']
-    participants_row['Win'] = row['stats']['win']
-    participants_row['Kills'] = row['stats']['kills']
-    participants_row['Deaths'] = row['stats']['deaths']
-    participants_row['Assists'] = row['stats']['assists']
-    participants_row['Damage'] = row['stats']['totalDamageDealt']
-    # participants_row['Gold'] = row['stats']['goldEarned']
-    # participants_row['champLevel'] = row['stats']['champLevel']
-    # participants_row['Minions'] = row['stats']['totalMinionsKilled']
-    # participants_row['item0'] = row['stats']['item0']
-    # participants_row['item1'] = row['stats']['item1']
-    participants.append(participants_row)
-  df = pd.DataFrame(participants)
-  return df
-  
-
-      
+    participants = []
+    for row in match_detail["participants"]:
+        participants_row = {}
+        # Get summoner name
+        pid = row["participantId"]
+        summonerInfo = pydash.find(
+            match_detail["participantIdentities"], {"participantId": pid}
+        )
+        participants_row["Name"] = summonerInfo["player"]["summonerName"]
+        participants_row["Champion"] = champ_dict[str(row["championId"])]
+        # participants_row['spell1'] = row['spell1Id']
+        # participants_row['spell2'] = row['spell2Id']
+        participants_row["Win"] = row["stats"]["win"]
+        participants_row["Kills"] = row["stats"]["kills"]
+        participants_row["Deaths"] = row["stats"]["deaths"]
+        participants_row["Assists"] = row["stats"]["assists"]
+        participants_row["Damage"] = row["stats"]["totalDamageDealt"]
+        # participants_row['Gold'] = row['stats']['goldEarned']
+        # participants_row['champLevel'] = row['stats']['champLevel']
+        # participants_row['Minions'] = row['stats']['totalMinionsKilled']
+        # participants_row['item0'] = row['stats']['item0']
+        # participants_row['item1'] = row['stats']['item1']
+        participants.append(participants_row)
+    df = pd.DataFrame(participants)
+    return df
