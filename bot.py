38e33a5a82ee83c257106febc98e1be1fbb801f4
dhmoon91 <dhmoon91@gmail.com>
2021-07-02 03:58:14 -0400

    catch exception

--- bot.py
+++ bot.py
@@ -46,45 +46,48 @@ async def on_member_join(member):
 @bot.command(name="rank", help="Get rank of summoner")
 async def get_rank(ctx, name: str):
     """Sends the summoner's rank information to the bot"""
-    summoner_info = get_summoner_rank(name)
+    try:
+        summoner_info = get_summoner_rank(name)
 
-    embed = discord.Embed(title="Solo/Duo Rank", color=discord.Color.dark_gray())
+        embed = discord.Embed(title="Solo/Duo Rank", color=discord.Color.dark_gray())
 
-    summoner_name = summoner_info["user_name"]
+        summoner_name = summoner_info["user_name"]
 
-    # Removing space of the summoner name to access op.gg url of the summoner
-    summoner_name_opgg = summoner_name.replace(" ", "")
-    print(summoner_info["summoner_icon_image_url"])
-    # Add author, thumbnail, fields, and footer to the embed
-    embed.set_author(
-        name=summoner_name,
-        url=f"https://na.op.gg/summoner/userName={summoner_name_opgg}",
-        icon_url=summoner_info["summoner_icon_image_url"],
-    )
+        # Removing space of the summoner name to access op.gg url of the summoner
+        summoner_name_opgg = summoner_name.replace(" ", "")
+        print(summoner_info["summoner_icon_image_url"])
+        # Add author, thumbnail, fields, and footer to the embed
+        embed.set_author(
+            name=summoner_name,
+            url=f"https://na.op.gg/summoner/userName={summoner_name_opgg}",
+            icon_url=summoner_info["summoner_icon_image_url"],
+        )
 
-    # Get image of tier by path
-    tier_image = summoner_info["tier_image"]
-    file = discord.File(tier_image)
-    # Need to get the filename in order to attach to the thumbnail
-    tier_image_filename = tier_image.replace("ranked-emblems/", "")
-    # Embed thumbnail image of tier at the side of the embed
-    embed.set_thumbnail(url=f"attachment://{tier_image_filename}")
+        # Get image of tier by path
+        tier_image = summoner_info["tier_image"]
+        file = discord.File(tier_image)
+        # Need to get the filename in order to attach to the thumbnail
+        tier_image_filename = tier_image.replace("ranked-emblems/", "")
+        # Embed thumbnail image of tier at the side of the embed
+        embed.set_thumbnail(url=f"attachment://{tier_image_filename}")
 
-    # Setting variables for summoner information to display as field
-    summoner_rank = summoner_info["tier"]
-    solo_win = summoner_info["solo_win"]
-    solo_loss = summoner_info["solo_loss"]
-    summoner_total_game = solo_win + solo_loss
-    solo_rank_percentage = int(solo_win / summoner_total_game * 100)
+        # Setting variables for summoner information to display as field
+        summoner_rank = summoner_info["tier"]
+        solo_win = summoner_info["solo_win"]
+        solo_loss = summoner_info["solo_loss"]
+        summoner_total_game = solo_win + solo_loss
+        solo_rank_percentage = int(solo_win / summoner_total_game * 100)
 
-    embed.add_field(
-        name=summoner_rank,
-        value="Total Games Played:"
-        + f"{summoner_total_game}\n{solo_win}W \
-          {solo_loss}L {solo_rank_percentage}%",
-        inline=False,
-    )
-    await ctx.send(file=file, embed=embed)
+        embed.add_field(
+            name=summoner_rank,
+            value="Total Games Played:"
+            + f"{summoner_total_game}\n{solo_win}W \
+            {solo_loss}L {solo_rank_percentage}%",
+            inline=False,
+        )
+        await ctx.send(file=file, embed=embed)
+    except Exception as e:
+        print(e)
 
 
 @bot.command(name="last_match", help="Get last match history")
