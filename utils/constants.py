4dea310977b689b4a254363afe47eada6f65b17c
jung1411 <jung1411@gmail.com>
2021-07-18 03:09:06 -0400

    merged master

--- utils/constants.py
+++ utils/constants.py
@@ -26,9 +26,14 @@ TIER_VALUE = {
     "GOLD": 4,
     "PLATINUM": 5,
     "DIAMOND": 6,
+    # master, grandmaster, challenger only have lp points starting 0lp for master,
+    # around 300lp for grandmaster and 650lp for master
+    # these are not fixed lp points as they are assigned to GM or Challenger
+    # depending on the ranking not based on how much lp points they have
+    # Challenger: 1 ~ 300, GrandMaster: 301 ~ 1000
     "MASTER": 7,
-    "GRANDMASTER": 8,
-    "CHALLENGER": 9,
+    "GRANDMASTER": 7,
+    "CHALLENGER": 7,
 }
 
 RANK_VALUE = {"I": 0.75, "II": 0.5, "III": 0.25, "IV": 0}
