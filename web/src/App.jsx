f24c14f3935b127b888bdb3b3eb279c7d9d30456
dhmoon91 <dhmoon91@gmail.com>
2021-07-25 19:26:38 -0400

    setup web

---
+++ web/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+  <>
+    <Header/>
+  </>
+  );
+}
+
+export default App;
